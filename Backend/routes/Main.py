e36a6e4f458c2a43fbc3416ed7af63f191947101
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2024-01-30 10:05:13 +0100

    Added basic functions

--- Backend/routes/Main.py
+++ Backend/routes/Main.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from Wrapper.middleCrud import createToken
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key' 
@@ -10,3 +12,16 @@ app.register_blueprint(Form, url_prefix="/form")
 @app.route("/")
 def index():
     return jsonify("hi")
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.json.get("name")
+    password = request.json.get("password")
+    
+    token = createToken(name, password)
+    # TODO: Make better token
+    
+    return {"status": "ok", "token": token}
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 8000)
--- Backend/routes/Wrapper/middleCrud.py
+++ Backend/routes/Wrapper/middleCrud.py
@@ -1,3 +1,10 @@
+import base64
+
 def missing_params(*params):
     return not (None in params)
 
+def createToken(username, password):
+    passname = username + password
+    token = base64.b64encode(passname)
+    return str(token)
+    
